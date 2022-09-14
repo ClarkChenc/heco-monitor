60f64be6c82733199321d5728ee2f873fe1dc9fc
zhang0125 <jell7@163.com>
2022-09-14 19:04:44 +0800

    add a goroutine process contract call

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -39,51 +40,57 @@ func OnTick() {
 		return
 	}
 
-	for _, token := range dao.TokenMap {
-		if token.EthAddress != "0x0000000000000000000000000000000000000001" {
-			ethContract, _ := erc20.NewErc20(common.HexToAddress(token.EthAddress), ethClient)
-			amount, _ := ethContract.BalanceOf(nil, common.HexToAddress("0xa929022c9107643515f5c777ce9a910f0d1e490c"))
-			token.EthAmount = decimal.NewFromBigInt(amount, 0)
-			decimals, _ := ethContract.Decimals(nil)
-			token.EthDecimals = int(decimals)
+	var wg sync.WaitGroup
+	for _, t := range dao.TokenMap {
+		wg.Add(1)
+		go func(token dao.Token) {
+			defer wg.Done()
+			if token.EthAddress != "0x0000000000000000000000000000000000000001" {
+				ethContract, _ := erc20.NewErc20(common.HexToAddress(token.EthAddress), ethClient)
+				amount, _ := ethContract.BalanceOf(nil, common.HexToAddress("0xa929022c9107643515f5c777ce9a910f0d1e490c"))
+				token.EthAmount = decimal.NewFromBigInt(amount, 0)
+				decimals, _ := ethContract.Decimals(nil)
+				token.EthDecimals = int(decimals)
 
-		} else {
-			amount, _ := ethClient.BalanceAt(context.Background(), common.HexToAddress("0xa929022c9107643515f5c777ce9a910f0d1e490c"), nil)
-			token.EthAmount = decimal.NewFromBigInt(amount, 0)
-			token.EthDecimals = 18
-		}
+			} else {
+				amount, _ := ethClient.BalanceAt(context.Background(), common.HexToAddress("0xa929022c9107643515f5c777ce9a910f0d1e490c"), nil)
+				token.EthAmount = decimal.NewFromBigInt(amount, 0)
+				token.EthDecimals = 18
+			}
 
-		hecoContract, _ := erc20.NewErc20(common.HexToAddress(token.HecoAddress), hecoClient)
-		hecoAmount, _ := hecoContract.TotalSupply(nil)
-		token.HecoAmount = decimal.NewFromBigInt(hecoAmount, 0)
-		hecoDecimals, _ := hecoContract.Decimals(nil)
-		token.HecoDecimals = int(hecoDecimals)
+			hecoContract, _ := erc20.NewErc20(common.HexToAddress(token.HecoAddress), hecoClient)
+			hecoAmount, _ := hecoContract.TotalSupply(nil)
+			token.HecoAmount = decimal.NewFromBigInt(hecoAmount, 0)
+			hecoDecimals, _ := hecoContract.Decimals(nil)
+			token.HecoDecimals = int(hecoDecimals)
 
-		eAmount := (float64)(hecoAmount.Div(hecoAmount,
-			big.NewInt(10).Exp(big.NewInt(10), big.NewInt(int64(token.EthDecimals)-3), nil)).Uint64()) / 1000
-		influx.WriteRecord("bridge",
-			map[string]string{"chain": "eth", "token": token.TokenName},
-			map[string]interface{}{"amount": eAmount})
+			eAmount := (float64)(hecoAmount.Div(hecoAmount,
+				big.NewInt(10).Exp(big.NewInt(10), big.NewInt(int64(token.EthDecimals)-3), nil)).Uint64()) / 1000
+			influx.WriteRecord("bridge",
+				map[string]string{"chain": "eth", "token": token.TokenName},
+				map[string]interface{}{"amount": eAmount})
 
-		hAmount := (float64)(hecoAmount.Div(hecoAmount,
-			big.NewInt(10).Exp(big.NewInt(10), big.NewInt(int64(token.HecoDecimals)-3), nil)).Uint64()) / 1000
-		influx.WriteRecord("bridge",
-			map[string]string{"chain": "heco", "token": token.TokenName},
-			map[string]interface{}{"amount": hAmount})
+			hAmount := (float64)(hecoAmount.Div(hecoAmount,
+				big.NewInt(10).Exp(big.NewInt(10), big.NewInt(int64(token.HecoDecimals)-3), nil)).Uint64()) / 1000
+			influx.WriteRecord("bridge",
+				map[string]string{"chain": "heco", "token": token.TokenName},
+				map[string]interface{}{"amount": hAmount})
 
-		influx.WriteRecord("bridge",
-			map[string]string{"chain": "diff", "token": token.TokenName},
-			map[string]interface{}{"amount": eAmount - hAmount})
-		if err := token.Update(util.GetDB()); err != nil {
-			fmt.Println(err)
-		}
+			influx.WriteRecord("bridge",
+				map[string]string{"chain": "diff", "token": token.TokenName},
+				map[string]interface{}{"amount": eAmount - hAmount})
+			if err := token.Update(util.GetDB()); err != nil {
+				fmt.Println(err)
+			}
+		}(t)
 	}
+	wg.Wait()
 	fmt.Println("update successfully")
 }
 
 func main() {
 	ctx := context.Background()
-	ticker := time.NewTicker(10 * time.Second)
+	ticker := time.NewTicker(1 * time.Minute)
 	defer ticker.Stop()
 
 	for {
